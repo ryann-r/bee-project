19749f9893304b4b54657ac755a155680c46f85a
Ryann Rourk <ryannrourk@gmail.com>
2020-10-11 17:32:24 -0400

    Set up files, parsing file, data to parse

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash, session, redirect, jsonify
+from model import connect_to_db
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
